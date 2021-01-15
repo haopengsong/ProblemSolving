bd6dfdb1c9f92c5f75d480eff20501b2592ddeeb
haopengsong <jsong2018@icloud.com>
2021-01-15 23:55:16 +0800

    jiangzhi-58

---
+++ stringReverseLeft.py
@@ -0,0 +1,9 @@
+def reverseLeftKcharToTheEnd(s, k):
+	res = ""
+	for i in range(k, k+len(s)):
+		res += s[i % len(s)]
+	return res
+
+x = input()
+y = input()
+print(reverseLeftKcharToTheEnd(str(x),int(y)))
